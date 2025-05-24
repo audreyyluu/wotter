8ece6ada0f90dab8aae68a2c675841630f822c03
Audrey Lu <audreyylu04@gmail.com>
2025-05-23 21:27:17 -0700

    Added buttons to the wotter bottle page and stamp card page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-start', margin: '1rem' }}>
+        <a href="/bottles">
+          <button>Wotter Bottles</button>
+        </a>
+      </div>
       <p>Main page</p>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end', position: 'fixed', bottom: 0, margin: '1rem' }}>
+        <a href="/stamp">
+          <button>Stamp Card</button>
+        </a>
+      </div>
     </div>
   );
 }
