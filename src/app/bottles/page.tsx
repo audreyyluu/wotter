721f0aa5c88fcd114a739c2bde1abc3fcfdbc4b2
rachelmgk <99305864+rachelmgk@users.noreply.github.com>
2025-05-23 21:40:39 -0700

    home button

--- src/app/bottles/page.tsx
+++ src/app/bottles/page.tsx
@@ -2,6 +2,11 @@ export default function Bottles() {
   return (
     <div>
       <p>Bottles page</p>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end', position: 'fixed', bottom: 0, margin: '1rem' }}>
+        <a href="/">
+          <button>Go Home</button>
+        </a>
+      </div>
     </div>
   );
 }
--- src/app/stamp/page.tsx
+++ src/app/stamp/page.tsx
@@ -2,6 +2,11 @@ export default function Stamp() {
   return (
     <div>
       <p>Stamp card page</p>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end', position: 'fixed', bottom: 0, margin: '1rem' }}>
+        <a href="/">
+          <button>Go Home</button>
+        </a>
+      </div>
     </div>
   );
 }
